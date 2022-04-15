4ba3b75d32721084488902ac3e6f44c1b3a46794
Minh <102460609+minh2k2@users.noreply.github.com>
2022-04-15 13:36:19 +0700

    call api

--- App.tsx
+++ App.tsx
@@ -10,51 +10,6 @@ import {
   View,
 } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-const DATA = [
-  {
-    albumId: 1,
-    id: 1,
-    title: 'accusamus beatae ad facilis cum similique qui sunt',
-    url: 'https://via.placeholder.com/600/92c952',
-    thumbnailUrl: 'https://via.placeholder.com/150/92c952',
-  },
-  {
-    albumId: 1,
-    id: 2,
-    title: 'reprehenderit est deserunt velit ipsam',
-    url: 'https://via.placeholder.com/600/771796',
-    thumbnailUrl: 'https://via.placeholder.com/150/771796',
-  },
-  {
-    albumId: 1,
-    id: 3,
-    title: 'officia porro iure quia iusto qui ipsa ut modi',
-    url: 'https://via.placeholder.com/600/24f355',
-    thumbnailUrl: 'https://via.placeholder.com/150/24f355',
-  },
-  {
-    albumId: 1,
-    id: 4,
-    title: 'culpa odio esse rerum omnis laboriosam voluptate repudiandae',
-    url: 'https://via.placeholder.com/600/d32776',
-    thumbnailUrl: 'https://via.placeholder.com/150/d32776',
-  },
-  {
-    albumId: 1,
-    id: 5,
-    title: 'natus nisi omnis corporis facere molestiae rerum in',
-    url: 'https://via.placeholder.com/600/f66b97',
-    thumbnailUrl: 'https://via.placeholder.com/150/f66b97',
-  },
-  {
-    albumId: 1,
-    id: 6,
-    title: 'accusamus ea aliquid et amet sequi nemo',
-    url: 'https://via.placeholder.com/600/56a8c2',
-    thumbnailUrl: 'https://via.placeholder.com/150/56a8c2',
-  },
-];
-
 const Item = ({item, onPress, backgroundColor, textColor}) => (
   <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
     <Text style={[styles.title, textColor]}>{item.title}</Text>
@@ -63,10 +18,27 @@ const Item = ({item, onPress, backgroundColor, textColor}) => (
 
 const App = () => {
   const [selectedId, setSelectedId] = useState(null);
-
+  const [data, setdata] = useState([]);
+  useEffect(() => {
+    getListPhotos();
+    return () => {};
+  }, []);
+  const getListPhotos = () => {
+    const apiURL =
+      'https://jsonplaceholder.typicode.com/photos?_limit=6&_page=1';
+    fetch(apiURL)
+      .then(res => res.json())
+      .then(resJson => {
+        setdata(resJson);
+      })
+      .catch(error => {
+        console.log('Error:', error);
+      });
+  };
   const renderItem = ({item, index}) => {
-    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
-    const color = item.id === selectedId ? 'white' : 'black';
+    const backgroundColor =
+      item.getListPhotos === selectedId ? '#6e3b6e' : '#f9c2ff';
+    const color = item.getListPhotos === selectedId ? 'white' : 'black';
 
     return (
       <View style={styles.item}>
@@ -92,7 +64,7 @@ const App = () => {
     <NavigationContainer>
       <SafeAreaView style={styles.container}>
         <FlatList
-          data={DATA}
+          data={data}
           renderItem={renderItem}
           keyExtractor={item => item.id}
           extraData={selectedId}
--- stack/man1.tsx
+++ stack/man1.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import {
   FlatList,
   SafeAreaView,
@@ -10,50 +10,7 @@ import {
   Image,
   View,
 } from 'react-native';
-const DATA = [
-  {
-    albumId: 1,
-    id: 1,
-    title: 'accusamus beatae ad facilis cum similique qui sunt',
-    url: 'https://via.placeholder.com/600/92c952',
-    thumbnailUrl: 'https://via.placeholder.com/150/92c952',
-  },
-  {
-    albumId: 1,
-    id: 2,
-    title: 'reprehenderit est deserunt velit ipsam',
-    url: 'https://via.placeholder.com/600/771796',
-    thumbnailUrl: 'https://via.placeholder.com/150/771796',
-  },
-  {
-    albumId: 1,
-    id: 3,
-    title: 'officia porro iure quia iusto qui ipsa ut modi',
-    url: 'https://via.placeholder.com/600/24f355',
-    thumbnailUrl: 'https://via.placeholder.com/150/24f355',
-  },
-  {
-    albumId: 1,
-    id: 4,
-    title: 'culpa odio esse rerum omnis laboriosam voluptate repudiandae',
-    url: 'https://via.placeholder.com/600/d32776',
-    thumbnailUrl: 'https://via.placeholder.com/150/d32776',
-  },
-  {
-    albumId: 1,
-    id: 5,
-    title: 'natus nisi omnis corporis facere molestiae rerum in',
-    url: 'https://via.placeholder.com/600/f66b97',
-    thumbnailUrl: 'https://via.placeholder.com/150/f66b97',
-  },
-  {
-    albumId: 1,
-    id: 6,
-    title: 'accusamus ea aliquid et amet sequi nemo',
-    url: 'https://via.placeholder.com/600/56a8c2',
-    thumbnailUrl: 'https://via.placeholder.com/150/56a8c2',
-  },
-];
+
 
 const Item = ({item, onPress, backgroundColor, textColor}) => (
   <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
@@ -63,10 +20,27 @@ const Item = ({item, onPress, backgroundColor, textColor}) => (
 
 const App1 = ({navigation}) => {
   const [selectedId, setSelectedId] = useState(null);
-
+  const [data, setdata] = useState([]);
+  useEffect(() => {
+    getListPhotos();
+    return () => {};
+  }, []);
+  const getListPhotos = () => {
+    const apiURL =
+      'https://jsonplaceholder.typicode.com/photos?_limit=6&_page=1';
+    fetch(apiURL)
+      .then(res => res.json())
+      .then(resJson => {
+        setdata(resJson);
+      })
+      .catch(error => {
+        console.log('Error:', error);
+      });
+  };
   const renderItem = ({item, index}) => {
-    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
-    const color = item.id === selectedId ? 'white' : 'black';
+    const backgroundColor =
+      item.getListPhotos === selectedId ? '#6e3b6e' : '#f9c2ff';
+    const color = item.getListPhotos === selectedId ? 'white' : 'black';
 
     return (
       <View style={styles.item}>
@@ -91,7 +65,7 @@ const App1 = ({navigation}) => {
   return (
       <SafeAreaView style={styles.container}>
         <FlatList
-          data={DATA}
+          data={data}
           renderItem={renderItem}
           keyExtractor={item => item.id}
           extraData={selectedId}
